2adddcb4f335a0cb8732f4cf4e3418fde96fe894
Mateusz <mateusz@MacBook-Air-Mateusz.local>
2020-11-18 03:28:31 +0100

    code update

--- lightbox/main.js
+++ lightbox/main.js
@@ -1,16 +1,17 @@
-function showLightbox(img){
+function showLightbox(ev){
     const lightbox = document.querySelector('.lightbox');
     lightbox.classList.add('lightboxActive');
     const imgLink = document.querySelector('.lightbox img');
-    const imgUrl = img;
+    const imgUrl = ev.target.src;
     imgLink.src = imgUrl;
+    console.log(imgUrl);
 }
 
-const images = document.querySelector('.gallery img');
+const images = document.querySelectorAll('.gallery img');
 const imagesCount = images.length;
 for(let i = 0; i < imagesCount; i++) {
     const img = images[i];
-    img.addEventListener("click", showLightbox(img))
+    img.addEventListener('click', showLightbox)
 }
 
 
