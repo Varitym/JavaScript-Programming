1ea0cb2a6e8a93567fac249e6cafcc5eb4c55514
Mateusz <mateusz@MacBook-Air-Mateusz.local>
2021-01-21 12:03:26 +0100

    Edit code

--- lightbox/main.js
+++ lightbox/main.js
@@ -1,21 +1,66 @@
-function showLightbox(ev){
-    const lightbox = document.querySelector('.lightbox');
-    lightbox.classList.add('lightboxActive');
-    const imgLink = document.querySelector('.lightbox img');
-    const imgUrl = ev.target.src;
-    imgLink.src = imgUrl;
-    
-    lightbox.addEventListener('click', function hideLightbox(){
-    if(ev.target!==imgUrl)lightbox.classList.remove('lightboxActive')});
-    
-}
-
 const images = document.querySelectorAll('.gallery img');
-const imagesCount = images.length;
-for(let i = 0; i < imagesCount; i++) {
-    const img = images[i];
-    img.addEventListener('click', showLightbox)
+const lightbox = document.querySelector('.lightbox');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+const img = document.querySelector('.lightbox-img');
+
+let indexImg;
+let currentImg;
+
+function showLightBox(ev) {
+    lightbox.classList.add('visible');
+
+    currentImg = ev.target;
+    indexImg = parseInt(ev.target.dataset.index);
+
+    setImageSrc(currentImg);
+    toggleButtons();
 }
 
+function hideLightbox(ev) {
+    if (ev.target.classList.contains('lightbox')) {
+        lightbox.classList.remove('visible');
+    }
+}
 
+function setImageSrc(currentImg) {
+    let imgUrl = currentImg.src;
+    img.src = imgUrl;
+}
 
+function changeImage(ev) {
+    const btn = ev.target;
+
+    if (btn == nextBtn || btn.parentElement == nextBtn) {
+        indexImg++;
+    } else if (btn == prevBtn || btn.parentElement == prevBtn) {
+        indexImg--;
+    }
+
+    const change = document.querySelector(`[data-index="${indexImg}"]`);
+
+    setImageSrc(change);
+    toggleButtons();
+}
+
+function toggleButtons() {
+    prevBtn.classList.remove('hide');
+    nextBtn.classList.remove('hide');
+
+    if (indexImg == 1) {
+        prevBtn.classList.add('hide');
+    } else if (indexImg == images.length) {
+        nextBtn.classList.add('hide');
+    }
+}
+
+//event listeners
+
+for (let idx = 0; idx < images.length; idx++) {
+    const img = images[idx];
+    img.addEventListener('click', showLightBox);
+}
+
+lightbox.addEventListener('click', hideLightbox);
+nextBtn.addEventListener('click', changeImage);
+prevBtn.addEventListener('click', changeImage);
